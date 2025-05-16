9f9db830d197f33ae298bc336ab0de4c055f952d
Ayesha-Arshad-Ai <ayeshaharis2003@gmail.com>
2025-05-16 21:55:51 +0500

    frontend added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import PostForm from "./pages/UploadPost";
+import Chatbot from "./pages/Chatbot";
+import Login from "./pages/Login";  // Import Login component
+import Signup from "./pages/Signup";  // Import Signup component
+import PostPage from "./pages/Posts";
+import Profile from "./pages/Profile";
+import { TokenProvider } from "./context/TokenContext"; // Adjust path as needed
+import CyberTools from "./pages/Cybertools"
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <TokenProvider>
+    <Router>
+      <Routes>
+        {/* Home Page */}
+        <Route path="/" element={<Home />} />
+
+        {/* Generate Design Page */}
+        <Route path="/text-post" element={<PostForm />} />
+
+        {/* Chatbot Page */}
+        <Route path="/chatbot" element={<Chatbot />} />
+
+        {/* Login Page */}
+        <Route path="/login" element={<Login />} />
+         {/* Login Page */}
+        <Route path="/posts" element={<PostPage />} />
+
+
+        {/* Signup Page */}
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/edit_profile" element={<Profile />} />
+
+        <Route path="/tools" element={<CyberTools />} />
+
+
+
+
+      </Routes>
+    </Router>
+      </TokenProvider>
   );
-}
+};
 
 export default App;
